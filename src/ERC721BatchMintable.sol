01374ac9bda21e09a4b98f1f2876fb5229ed3edf
Tadas Varanauskas <tadas@varanauskas.lt>
2025-08-12 13:31:50 +0300

    Implement contract

---
+++ src/ERC721BatchMintable.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract ERC721BatchMintable is ERC721, ERC721Enumerable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 private _nextTokenId;
+    string private __baseURI;
+    address public owner;
+
+    constructor(string memory name, string memory symbol, string memory baseURI) ERC721(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        owner = msg.sender;
+        __baseURI = baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return __baseURI;
+    }
+
+    function safeMint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        for (uint256 index = 0; index < amount; index++)
+            _safeMint(to, _nextTokenId++);
+    }
+
+    function setOwner(address newOwner) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        owner = newOwner;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
